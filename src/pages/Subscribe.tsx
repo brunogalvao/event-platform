909c8e6facaa40a9ba9f0d19c70c1d7653d4750b
Bruno Galvão <bruno_galvao@outlook.com>
2022-06-25 12:15:59 -0300

    ajuste subscribe

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -3,7 +3,6 @@ import { useState, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "../components/Logo";
 import { useCreateSubscriberMutation } from "../graphql/generated";
-import ImgBG from ''
 
 export default function Subscribe() {
 
